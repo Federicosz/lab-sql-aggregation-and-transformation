fc3f0ded0a41e86f5cd65443b69f0e3739e43145
Federicosz <f.sarmiento.zamo@gmail.com>
2023-12-20 10:14:16 -0600

    Update Solution_FSZ.sql

--- Solution_FSZ.sql
+++ Solution_FSZ.sql
@@ -1,14 +1,15 @@
 -- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 select max(length) as 'max_duration', min(length) as 'min_duration' from film;
 -- 1.2. Express the average movie duration in hours and minutes. 
-select floor(avg(length)/60) as 'hours'	,round(avg(length)-60*floor(avg(length)/60),0) as 'minutes' from film;
+select floor(avg(length)/60) as 'hours'	,round(avg(length)%60) as 'minutes', concat(floor(avg(length)/60),'h',round(avg(length)%60),'min'),sec_to_time(avg(length)*60) from film;
 -- 2.1 Calculate the number of days that the company has been operating.
 select datediff(max(rental_date),min(rental_date)) as '# of days open' from rental;
 -- 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
 select *,monthname(rental_date) as 'rental_month',dayname(rental_date) as 'rental_day' from rental;
 -- 2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week.
-select *,case when dayofweek(rental_date) between 1 and 5 then 'workday' else 'weekend' end as 'day_type' from rental;
--- select distinct day_type from rental; ???
+select *,case when dayofweek(rental_date) between 0 and 4 then 'workday' else 'weekend' end as 'day_type' from rental;
+/*select distinct day_type from rental; we don't know yet how to save it. however you can create other query to check it as select distinct dayname(rental_date),
+case when dayofweek(rental_date) between 0 and 4 then 'workday' else 'weekend' end as 'day_type' from rental*/ 
 -- You need to ensure that customers can easily access information about the movie collection. To achieve this, retrieve the film titles and their rental duration.
 -- If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
 select title, case when rental_duration is null then 'Not Available' else rental_duration end as rental_duration from film order by rental_duration desc;
@@ -32,4 +33,4 @@ select distinct rating, round(avg(length),2) from film group by rating order by 
 -- 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
 select distinct rating, round(avg(length),2) from film group by rating having avg(length)>120;
 -- Bonus: determine which last names are not repeated in the table actor.
-select distinct last_name, count(last_name) from actor group by last_name having count(last_name)=1;
+select last_name, count(last_name) from actor group by last_name having count(last_name)=1;
